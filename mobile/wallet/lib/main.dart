257538ead7602998f6e16144e2aad25ac5bdd3cb
ArtZab0 <zabarovart@gmail.com>
2021-11-17 14:26:49 -0500

    desktop copy

--- mobile/wallet/lib/main.dart
+++ mobile/wallet/lib/main.dart
@@ -1,11 +1,11 @@
-import 'dart:html';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+// import 'package:http/http.dart';
 import 'package:wallet/screens/authentication/login.dart';
 import 'package:wallet/screens/wrapper.dart';
 import 'package:wallet/screens/shared/shared.dart';
-import 'package:web3dart/web3dart.dart';
+// import 'package:web3dart/web3dart.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,22 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    Client httpClient;
-    Web3Client ethClient;
-    bool data = false;
-    int myAmount = 0;
-    final myAddress = "0x127Ff1D9560F7992911389BA181f695b38EE9399";
-
-    @override
-    void initState() {
-      super.initState();
-      httpClient = Client();
-      ethClient = Web3Client();
-    }
 
     return MaterialApp(
       title: "Crypto Wallet",
-      home: LoginPage(),
+      home: Wrapper(),
       theme: ThemeData(
         fontFamily: 'Ubuntu',
         colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue)
