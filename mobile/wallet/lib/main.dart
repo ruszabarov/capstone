b5dede00b183881792d2e9d568fe131ef75843fc
ArtZab0 <zabarovart@gmail.com>
2021-11-18 14:04:57 -0500

    backend1

--- mobile/wallet/lib/main.dart
+++ mobile/wallet/lib/main.dart
@@ -1,19 +1,18 @@
-// import 'dart:html';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
-// import 'package:http/http.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart';
 import 'package:wallet/screens/authentication/login.dart';
 import 'package:wallet/screens/wrapper.dart';
 import 'package:wallet/screens/shared/shared.dart';
-// import 'package:web3dart/web3dart.dart';
+import 'package:web3dart/web3dart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
       title: "Crypto Wallet",
       home: Wrapper(),
