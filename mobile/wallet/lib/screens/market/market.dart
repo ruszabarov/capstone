4d2d8e1061a3587341e519582cb8d9c00a0602ee
Ruslan Zabarov <ruszabarov@gmail.com>
2022-01-20 14:45:48 -0500

    market search (incomplete)

--- mobile/wallet/lib/screens/market/market.dart
+++ mobile/wallet/lib/screens/market/market.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallet/screens/market/api.dart';
 import 'market_card.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,35 +11,107 @@ class MarketPage extends StatefulWidget {
 }
 
 class _MarketPageState extends State<MarketPage> {
-  var marketList = [
-    'ethereum',
-    'tether',
-    'usd-coin',
-    'matic-network',
-    'shiba-inu',
+  List<StaticTokenInformation> tokenList = [
+    StaticTokenInformation(
+        "ethereum", 'assets/images/coin_logos/ethereum.webp'),
+    StaticTokenInformation("tether", 'assets/images/coin_logos/tether.webp'),
+    StaticTokenInformation(
+        "usd-coin", 'assets/images/coin_logos/usd-coin.webp'),
   ];
+
+  List displayedList = [];
+
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    displayedList = tokenList;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: new ListView.separated(
-            padding: EdgeInsets.all(25),
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox(
-                height: 15,
-              );
-            },
-            physics: BouncingScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            itemCount: marketList.length,
-            itemBuilder: (BuildContext ctxt, int index) {
-              return new MarketCard(marketList[index]);
-            },
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(25, 15, 25, 0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter Coin Name",
+                        hintStyle: TextStyle(color: Colors.black, fontSize: 16),
+                        fillColor: Colors.blue,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      searchOperation(_controller.text);
+                    },
+                    child: Icon(
+                      Icons.search,
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
+          Flexible(
+            child: ListView.separated(
+              padding: EdgeInsets.all(25),
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  height: 15,
+                );
+              },
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              itemCount: displayedList.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                print('alt');
+                return new MarketCard(displayedList[index]);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void searchOperation(String searchText) {
+    List searchResult = [];
+
+    for (int i = 0; i < tokenList.length; i++) {
+      StaticTokenInformation data = tokenList[i];
+      if (data.name.toLowerCase().contains(searchText.toLowerCase())) {
+        searchResult.add(data);
+      }
+    }
+
+    setState(() {
+      displayedList = searchResult;
+    });
+  }
+}
+
+class StaticTokenInformation {
+  final String name;
+  final String imagePath;
+
+  StaticTokenInformation(this.name, this.imagePath);
 }
