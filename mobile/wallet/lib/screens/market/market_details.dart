0e581d29b09ef2df86287b1e0957cf72b6b05787
Ruslan Zabarov <ruszabarov@gmail.com>
2022-02-01 14:24:06 -0500

    Display date based on interval

--- mobile/wallet/lib/screens/market/market_details.dart
+++ mobile/wallet/lib/screens/market/market_details.dart
@@ -25,7 +25,8 @@ class _MarketDetailsPageState extends State<MarketDetailsPage> {
   late ValueNotifier<String> currentCoinPrice;
   late ValueNotifier<DateTime> currentDate;
   late double lastPrice = 0;
-  bool displayHours = true;
+  bool displayMinutes = true;
+  bool displayHours = false;
 
   @override
   void initState() {
@@ -37,53 +38,6 @@ class _MarketDetailsPageState extends State<MarketDetailsPage> {
       enable: true,
       tooltipSettings: InteractiveTooltip(enable: false),
     );
-  }
-
-  _getCoinData(int days) async {
-    data = [];
-
-    List prices = await getMarketData(widget.coinName, days);
-    _calculateBounds(prices);
-
-    lastPrice = await getCoinData(widget.coinName)
-        .then((value) => value['current_price']);
-    currentCoinPrice = ValueNotifier<String>(lastPrice.toString());
-
-    String priceString = lastPrice.toString();
-    for (int i = 0; i < priceString.length; i++) {
-      decimalPlaces++;
-      if (priceString[i] == '.') {
-        decimalPlaces = 0;
-      }
-    }
-
-    _calculatePriceChange(prices);
-
-    setState(() {});
-  }
-
-  _calculateBounds(List prices) {
-    minPrice = prices[0][1];
-    maxPrice = prices[0][1];
-
-    prices.forEach((price) {
-      data.add(
-          LinearPrice(DateTime.fromMillisecondsSinceEpoch(price[0]), price[1]));
-
-      if (price[1] < minPrice) {
-        minPrice = price[1];
-      } else if (price[1] > maxPrice) {
-        maxPrice = price[1];
-      }
-    });
-  }
-
-  _calculatePriceChange(List prices) {
-    double firstPrice = prices[0][1];
-    double lastPrice = prices.last[1];
-
-    priceChangeInRange = lastPrice - firstPrice;
-    percentChangeInRange = (lastPrice / firstPrice) * 100 - 100;
   }
 
   @override
@@ -114,17 +68,25 @@ class _MarketDetailsPageState extends State<MarketDetailsPage> {
                   width: 15,
                 ),
                 ValueListenableBuilder(
-                  valueListenable: currentDate,
-                  builder: (context, DateTime value, child) => displayHours
-                      ? Text(
+                    valueListenable: currentDate,
+                    builder: (context, DateTime value, child) {
+                      if (displayMinutes) {
+                        return Text(
                           DateFormat('MMM d, yyyy').add_jm().format(value),
                           style: TextStyle(fontSize: 20),
-                        )
-                      : Text(
+                        );
+                      } else if (displayHours) {
+                        return Text(
+                          DateFormat('MMM d, yyyy h a').format(value),
+                          style: TextStyle(fontSize: 20),
+                        );
+                      } else {
+                        return Text(
                           DateFormat('MMM d, yyyy').format(value),
                           style: TextStyle(fontSize: 20),
-                        ),
-                ),
+                        );
+                      }
+                    }),
               ],
             ),
           ),
@@ -155,8 +117,8 @@ class _MarketDetailsPageState extends State<MarketDetailsPage> {
                     days: 90, label: "3M", getCoinData: _getCoinData),
                 TimeRangeButton(
                     days: 365, label: "1Y", getCoinData: _getCoinData),
-                // TimeRangeButton(
-                //     days: 'max', label: "All", getCoinData: _getCoinData),
+                TimeRangeButton(
+                    days: -1, label: "All", getCoinData: _getCoinData),
               ],
             ),
           )
@@ -169,6 +131,67 @@ class _MarketDetailsPageState extends State<MarketDetailsPage> {
   void dispose() {
     currentCoinPrice.dispose();
     super.dispose();
+  }
+
+  _getCoinData(int days) async {
+    data = [];
+
+    if (days == 1) {
+      displayMinutes = true;
+      displayHours = false;
+    } else if (days <= 30 && days != -1) {
+      displayHours = true;
+      displayMinutes = false;
+    } else {
+      displayMinutes = false;
+      displayHours = false;
+    }
+
+    List prices = await getMarketData(widget.coinName, days);
+
+    _calculateBounds(prices);
+
+    lastPrice = await getSimpleTokenData(widget.coinName)
+        .then((value) => value[widget.coinName]['usd']);
+
+    //Calculate decimal palces
+    String priceString = lastPrice.toString();
+    for (int i = 0; i < priceString.length; i++) {
+      decimalPlaces++;
+      if (priceString[i] == '.') {
+        decimalPlaces = 0;
+      }
+    }
+
+    currentCoinPrice = ValueNotifier<String>(lastPrice.toStringAsFixed(1));
+
+    _calculatePriceChange(prices);
+
+    setState(() {});
+  }
+
+  _calculateBounds(List prices) {
+    minPrice = prices[0][1];
+    maxPrice = prices[0][1];
+
+    prices.forEach((price) {
+      data.add(
+          LinearPrice(DateTime.fromMillisecondsSinceEpoch(price[0]), price[1]));
+
+      if (price[1] < minPrice) {
+        minPrice = price[1];
+      } else if (price[1] > maxPrice) {
+        maxPrice = price[1];
+      }
+    });
+  }
+
+  _calculatePriceChange(List prices) {
+    double firstPrice = prices[0][1];
+    double lastPrice = prices.last[1];
+
+    priceChangeInRange = lastPrice - firstPrice;
+    percentChangeInRange = (lastPrice / firstPrice) * 100 - 100;
   }
 }
 
