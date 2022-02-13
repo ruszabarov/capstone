f61318ec596a1193d554b66612805477e8a90b5f
Ruslan Zabarov <ruszabarov@gmail.com>
2022-02-13 00:22:45 -0500

    fixed backend bug

--- mobile/wallet/lib/tokenBalanceAPI.dart
+++ mobile/wallet/lib/tokenBalanceAPI.dart
@@ -15,41 +15,44 @@ import 'package:shared_preferences/shared_preferences.dart';
 final etherscan = EtherscanAPI(apiKey: apiKey, chain: EthChain.rinkeby);
 final myAddress = "0x127Ff1D9560F7992911389BA181f695b38EE9399";
 
-
-void getTokenList() async{
-  final String abi = await rootBundle.loadString("assets/build/contracts/token-list-rinkeby.json");
+void getTokenList() async {
+  final String abi = await rootBundle
+      .loadString("assets/build/contracts/token-list-rinkeby.json");
   final json = await jsonDecode(
     abi,
   );
   String getAddresses(int i) {
     return json[i]["address"] as String;
   }
-  for(int i = 0; i < json.length; i++) {
-    final balance = await etherscan.tokenBalance(address: myAddress, contractAddress: getAddresses(i));
+
+  for (int i = 0; i < json.length; i++) {
+    final balance = await etherscan.tokenBalance(
+        address: myAddress, contractAddress: getAddresses(i));
 
     await Future.delayed(Duration(seconds: 1));
     print(balance);
   }
-  
 }
 
 class Token {
-  String name = ""; 
-  String symbol = ""; 
-  String address = ""; 
+  String name = "";
+  String symbol = "";
+  String address = "";
   int decimal = 0;
-  Token({required this.name, required this.symbol, required this.address, required this.decimal});
+  Token(
+      {required this.name,
+      required this.symbol,
+      required this.address,
+      required this.decimal});
 
-        
   factory Token.fromJson(Map<dynamic, dynamic> parsedJson) {
     return new Token(
-      name: parsedJson['name'] ?? "",
-      symbol: parsedJson['symbol'] ?? "",
-      address: parsedJson['address'] ?? "",
-      decimal: parsedJson['decimal'] ?? ""
-      );
+        name: parsedJson['name'] ?? "",
+        symbol: parsedJson['symbol'] ?? "",
+        address: parsedJson['address'] ?? "",
+        decimal: parsedJson['decimal'] ?? "");
   }
-        
+
   Map<String, dynamic> toJson() {
     return {
       "name": this.name,
@@ -59,7 +62,6 @@ class Token {
     };
   }
 }
-
 
 abstract class ITokenService {
   void addToken(String name, String symbol, String address, int decimal);
@@ -72,21 +74,26 @@ class TokenService implements ITokenService {
   final SharedPreferences _preferences;
 
   @override
-  void addToken(String name, String symbol, String address, int decimals) async {
-  // use shared_preferences to store user preferences
-  SharedPreferences shared_User = await SharedPreferences.getInstance();
-  Map decode_options = jsonDecode(await rootBundle.loadString(""));
-  String tokens = jsonEncode(Token.fromJson(decode_options));
-  shared_User.setString('tokens', tokens);
-            
-  Map userMap = jsonDecode(shared_User.getString('tokens'));
-  var user = Token.fromJson(userMap);
-}
+  void addToken(
+      String name, String symbol, String address, int decimals) async {
+    // use shared_preferences to store user preferences
+    /*
+    SharedPreferences shared_User = await SharedPreferences.getInstance();
+    Map decode_options = jsonDecode(await rootBundle.loadString(""));
+    String tokens = jsonEncode(Token.fromJson(decode_options));
+    shared_User.setString('tokens', tokens);
+
+    Map userMap = jsonDecode(shared_User.getString('tokens'));
+    var user = Token.fromJson(userMap);
+
+     */
+  }
+
   // gets
   @override
   Map<String, dynamic>? getTokens() {
     String? userTokens = _preferences.getString('token');
-    if(userTokens == null) {
+    if (userTokens == null) {
       return null;
     }
     return jsonDecode(userTokens) as Map<String, dynamic>;
