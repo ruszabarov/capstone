6197f2c011c6b3e0e51a7135d73117ef5b0224f8
ArtZab0 <zabarovart@gmail.com>
2022-04-27 14:29:16 -0400

    add ether fixed

--- mobile/wallet/lib/configuration_service.dart
+++ mobile/wallet/lib/configuration_service.dart
@@ -193,7 +193,16 @@ class ConfigurationService implements IConfigurationService {
   Future<void> addEther(int id) async {
     
     if (await _preferences.getString('tokenList') == null) {
-      addEther(0);
+      List<Token> ether = [];
+      ether.add(Token(
+        id: 0,
+        name: "Ether",
+        symbol: "ETH",
+        address: "0x0000000000000000000000000000000000000000",
+        decimals: 18));
+
+    String encodedData = Token.encode(ether);
+    await _preferences.setString('tokenList', encodedData);
     }
 
     List<Token> tokenList = await Token.decode(await _preferences.getString('tokenList'));;
